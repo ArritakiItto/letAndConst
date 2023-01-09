eef305fe39cb168a810e35a190dc813209df7ff2
Khoa Le <106197609+ArritakiItto@users.noreply.github.com>
2023-01-08 18:19:49 -0800

    Add files via upload

---
+++ letAndConst.js
@@ -0,0 +1,24 @@
+//let and const Exercise
+//In this exercise, you’ll refactor some ES5 code into ES2015.
+
+//ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+ES2015 Global Constants
+/* Write an ES2015 Version */
+let PI = 3.14;
+PI = 42;
+
+//Quiz
+//What is the difference between var and let?
+//Var is global scope, let is function and block scoped
+//Var can be declared from anywhere which isn't recommended
+
+//What is the difference between var and const?
+//Var is global scope, let is function and block scoped
+
+//What is the difference between let and const?
+// let allows variable to be changed, const cannot be changed
+
+//What is hoisting?
+//when code is compiled variable declaration is ran first before initialization
